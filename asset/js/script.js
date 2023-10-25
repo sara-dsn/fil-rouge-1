698153cc993df7fdfaa544bdcdeb9256cdf46939
sara-dsn <sabr.tdg@gmail.com>
2023-10-25 16:46:10 +0200

    fin form

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,30 +1,95 @@
-$(document).ready(function(){
-    $("#envoyer").click(function(){
-        alert("a");
+$(document).ready(function () {
+
+    $("#monbouton").click(function () {
+     
         verif();
-    } );
-    function verif(){
-        var envoie=true;
-var nom= $("#n").val();
-if(nom===""){
-    envoi=false;
-    $("#n").show();
-    e.preventDefault();
-}
-else if(envoi=true){
-    $("#n").hide();
-    document.forms[0].submit();
-};
-alert(nom);
-var tel=$("#t").val();
-if(tel===""){
-    envoie=false;
-    $("#tel").show();
-    e.preventDefault();
-    }
-    else if (envoie=true){
-        $("#tel").hide();
-        document.forms[0].submit();
-    }
-};
     });
+    $("#bouton").click(function () {
+       
+        ver();
+    });
+
+
+    function verif() {
+   
+
+       
+        var envoi = true;
+        var nom = $("#n").val();
+        if (nom === "") {
+            envoi = false;
+            $("#nom").show();
+            e.preventDefault();
+        }
+        else if (envoi == true) {
+            $("#nom").hide();
+           
+        };
+        var tel = $("#t").val();
+        if (tel === "") {
+          
+            envoie = false;
+            $("#tel").show();
+            e.preventDefault();
+        }
+        else if (envoi == true) {
+            $("#tel").hide();
+          
+        };
+    };
+
+
+    function ver() {
+       
+        var envoi = true;
+
+        var np = $("#np").val();
+        if (np === "") {
+            envoi = false;
+            $("#nompre").show();
+            e.preventDefault();
+        }
+        else if (envoi == true) {
+            $("#nompre").hide();
+           
+        };
+
+
+        var email = $("#e").val();
+        if (email === "") {
+            envoi = false;
+            $("#email").show();
+            e.preventDefault();
+        }
+        else if (envoi == true) {
+            $("#email").hide();
+       
+        };
+
+
+        var tel = $("#t").val();
+        if (tel === "") {
+            envoi = false;
+            $("#tel").show();
+            e.preventDefault();
+        }
+        else if (envoi == true) {
+            $("#tel").hide();
+          
+        };
+
+        var adresse = $("#a").val();
+        if (adresse === "") {
+            envoi = false;
+            $("#adresse").show();
+            e.preventDefault();
+        }
+        else if (envoi == true) {
+            $("#adresse").hide();
+           
+        };
+
+        
+    };
+
+});
